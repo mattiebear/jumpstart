b9abdadc4a7daf6b1aa535581703f8815c5c96ba
Matthew Sells <matthew.sells@proton.me>
2024-04-29 18:45:41 -0500

    enable additional tests

--- lib/jumpstart_web/live/auth/user_confirmation_live.ex
+++ lib/jumpstart_web/live/auth/user_confirmation_live.ex
@@ -25,7 +25,7 @@ defmodule JumpstartWeb.Auth.UserConfirmationLive do
 
   def mount(%{"token" => token}, _session, socket) do
     form = to_form(%{"token" => token}, as: "user")
-    {:ok, assign(socket, form: form), temporary_assigns: [form: nil]}
+    {:ok, assign(socket, form: form), temporary_assigns: [form: nil], layout: false}
   end
 
   # Do not log in the user after confirmation to avoid a
